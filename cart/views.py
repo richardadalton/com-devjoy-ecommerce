97c9224c9bac72242325e8403af54bdc087925e0
Richard Dalton <richard@codeinstitute.net>
2018-11-21 12:36:10 +0000

    Cart total and remove from cart

--- cart/views.py
+++ cart/views.py
@@ -15,15 +15,27 @@ def add_to_cart(request):
     return redirect("/")
     
     
+def remove_from_cart(request):
+    product_id = request.POST['product']
+
+    cart = request.session.get('cart', {})
+    del cart[product_id]
+
+    request.session['cart'] = cart
+
+    return redirect("/cart/view/")    
+   
+   
     
 def view_cart(request):
     cart = request.session.get('cart', {})
     
-    
     cart_items = []
+    cart_total = 0
+    
     for product_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=product_id)
-    
+        item_total = product.price * quantity
         cart_items.append({
             'id': product.id,
             'name': product.name,
@@ -34,8 +46,9 @@ def view_cart(request):
             'price': product.price,
             'stock': product.stock,
             'quantity': quantity,
-            'total': product.price * quantity
-        })    
-    
-    return render(request, "cart/view_cart.html", {'cart_items': cart_items})
+            'total': item_total
+        })
+        cart_total += item_total
+
+    return render(request, "cart/view_cart.html", {'cart_items': cart_items, 'cart_total': cart_total})
 
